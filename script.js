e76bd8e4535709e1f60113367307d0f2c661e50c
borazan <boratuncel@gmail.com>
2022-03-12 15:24:31 +0100

    add score counters

--- script.js
+++ script.js
@@ -1,45 +1,5 @@
-let computerPlay = function () {
-  let decider = Math.random();
-  if (decider < 1 / 3) {
-    return "rock";
-  } else if (decider < 2 / 3) {
-    return "paper";
-  } else return "scissors";
-};
-
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection == "rock") {
-    switch (computerSelection) {
-      case "scissors":
-        return "You Win! Rock beats Scissors!";
-      case "paper":
-        return "You Lose! Paper beats Rock!";
-      default:
-        return "Draw!";
-    }
-  } else if (playerSelection == "paper") {
-    switch (computerSelection) {
-      case "scissors":
-        return "You Lose! Paper beats Rock!";
-      case "rock":
-        return "You Win! Paper beats Rock!";
-      default:
-        return "Draw!";
-    }
-  } else if (playerSelection == "scissors") {
-    switch (computerSelection) {
-      case "rock":
-        return "You Lose! Rock beats Scissors!";
-      case "paper":
-        return "You Win! Scissors beats Paper!";
-      default:
-        return "Draw!";
-    }
-  } else return "Invalid Selection!";
-}
-
-function game() {}
+var playerScore = 0;
+var computerScore = 0;
 
 const rockButton = document.createElement("button");
 rockButton.addEventListener("click", () => {
@@ -60,8 +20,8 @@ scissorsButton.innerText = "Scissors";
 const buttonContainer = document.createElement("div");
 buttonContainer.style.display = "flex";
 buttonContainer.style.justifyContent = "space-around";
-
 buttonContainer.append(rockButton, paperButton, scissorsButton);
+
 const body = document.querySelector("body");
 body.appendChild(buttonContainer);
 
@@ -69,6 +29,75 @@ const outputDiv = document.createElement("div");
 outputDiv.style.fontSize = "40px";
 outputDiv.innerText = "Click a button to start";
 body.appendChild(outputDiv);
+
+const scoreDiv = document.createElement("div");
+scoreDiv.style.fontSize = "60px";
+const playerScoreDiv = document.createElement("div");
+playerScoreDiv.innerHTML = playerScore;
+const computerScoreDiv = document.createElement("div");
+computerScoreDiv.innerHTML = computerScore;
+scoreDiv.append(playerScoreDiv, computerScoreDiv);
+body.appendChild(scoreDiv);
+
+let computerPlay = function () {
+  let decider = Math.random();
+  if (decider < 1 / 3) {
+    return "rock";
+  } else if (decider < 2 / 3) {
+    return "paper";
+  } else return "scissors";
+};
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection == "rock") {
+    switch (computerSelection) {
+      case "scissors":
+        playerWon();
+        return "You Win! Rock beats Scissors!";
+      case "paper":
+        computerWon();
+        return "You Lose! Paper beats Rock!";
+      default:
+        return "Draw!";
+    }
+  } else if (playerSelection == "paper") {
+    switch (computerSelection) {
+      case "scissors":
+        computerWon();
+        return "You Lose! Paper beats Rock!";
+      case "rock":
+        playerWon();
+        return "You Win! Paper beats Rock!";
+      default:
+        return "Draw!";
+    }
+  } else if (playerSelection == "scissors") {
+    switch (computerSelection) {
+      case "rock":
+        computerWon();
+        return "You Lose! Rock beats Scissors!";
+      case "paper":
+        playerWon();
+        return "You Win! Scissors beats Paper!";
+      default:
+        return "Draw!";
+    }
+  } else return "Invalid Selection!";
+}
+
+function playerWon() {
+  if (playerScore < 5) {
+    playerScore++;
+    playerScoreDiv.innerHTML = playerScore;
+}
+
+function computerWon() {
+  computerScore++;
+  computerScoreDiv.innerHTML = computerScore;
+}
+
+function game() {}
 
 function probability() {
   let result = [];
